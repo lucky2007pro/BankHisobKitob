a1fccf3dab4fa9d170cd6289e1be1bd38fb59944
lucky2007pro <usa20070302@gmail.com>
2026-04-24 21:09:48 +0500

    yangi

--- report/urls.py
+++ report/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import DashboardView, TransactionReportView, TransactionCreateView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
-    path('reports/', TransactionReportView.as_view(), name='transaction_report'),
-
+    path('reports/', TransactionReportView.as_view(), name='transaction_list'),
+    path('add/', TransactionCreateView.as_view(), name='transaction_add'),
 ]
